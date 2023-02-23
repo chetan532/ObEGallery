8e18d9f04e173a49b4da9342a46ab9ba973e2151
CHETANVAGHELA <chetanvaghela457@gmail.com>
2023-02-23 12:04:00 +0530

    Create Navigation Component , Create HomeGridFragment , Setup viewBinding for HomeGridFragment

---
+++ app/src/main/java/com/cv/obegallery/presentation/home/HomeGridFragment.kt
@@ -0,0 +1,33 @@
+package com.cv.obegallery.presentation.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.cv.obegallery.R
+import com.cv.obegallery.databinding.FragmentHomeGridBinding
+import com.cv.obegallery.presentation.main.MainViewModel
+
+class HomeGridFragment : Fragment() {
+    
+    private var _binding: FragmentHomeGridBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentHomeGridBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
